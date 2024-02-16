4754a10dac29ca99be389d4103a5d665db3f3eca
Cyril Kamgais <102966559+Kamgais@users.noreply.github.com>
2024-02-16 22:15:17 +0100

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import localFont from 'next/font/local'
 import "./globals.scss";
 import AppHeader from "./_components/appHeader/AppHeader";
@@ -13,7 +14,12 @@ const myFont = localFont({
   display: 'swap'
 })
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: "EU Legal Assist",
@@ -27,7 +33,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <AppHeader/>
         <main>{children}</main>
         <footer>
